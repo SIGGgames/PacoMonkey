25885dad4b772df62ca1173ba9c8e282e6bceab1
Aniol Serrano <aniol0012@gmail.com>
2023-11-26 18:41:52 +0100

    fix: fixed movment

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D _rigidbody2D;
+    public float moveSpeed = 1f;
+    public float jumpForce = 1f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool _isGrounded;
 
     private void Awake() {
         // Used to initialise an object's own reference
@@ -18,8 +24,65 @@ public class PlayerMovement : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown("space")) {
-            _rigidbody2D.velocity = new Vector2(0, 5);
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded) {
+            Jump();
+        }
+
+        LimitVelocity();
+    }
+
+
+    private void HandleMovement() {
+        bool moveRight = Input.GetKey(KeyCode.D);
+        bool moveLeft = Input.GetKey(KeyCode.A);
+        bool moveUp = Input.GetKeyDown(KeyCode.W);
+        bool moveDown = Input.GetKeyDown(KeyCode.S);
+
+        float moveX = 0f;
+        float moveY = 0f;
+
+        if (moveRight) {
+            moveX = 1;
+        }
+        else if (moveLeft) {
+            moveX = -1;
+        }
+
+        if (moveUp) {
+            moveY = 1;
+        }
+        else if (moveDown) {
+            moveY = -1;
+        }
+
+        Vector2 movement = new Vector2(moveX, moveY);
+
+        if (movement != Vector2.zero) {
+            movement = movement.normalized * moveSpeed;
+            _rigidbody2D.velocity = movement;
+        }
+    }
+
+
+    private void Jump() {
+        _rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    // Made to visualize the ground detector in the editor
+    private void OnDrawGizmosSelected() {
+        if (groundCheck == null) return;
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+    private void LimitVelocity() {
+        float maxSpeed = 5f;
+        if (_rigidbody2D.velocity.magnitude > maxSpeed) {
+            _rigidbody2D.velocity = Vector2.ClampMagnitude(_rigidbody2D.velocity, maxSpeed);
         }
     }
 }
