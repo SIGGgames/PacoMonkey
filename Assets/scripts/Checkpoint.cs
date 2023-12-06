cd9cac6cd5c4f434f0ffa5e7a781c2894edd06ff
Aniol Serrano <aniol0012@gmail.com>
2023-12-06 12:28:55 +0100

    fix: fixed camera

--- Assets/scripts/Checkpoint.cs
+++ Assets/scripts/Checkpoint.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
-    private GameMaster gm;
+    private GameMaster _gm;
     
     // Start is called before the first frame update
     void Start() {
-        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        _gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
-            gm.lastCheckPointPosition = transform.position;
+            _gm.lastCheckPointPosition = transform.position;
         }
     }
     // Update is called once per frame
