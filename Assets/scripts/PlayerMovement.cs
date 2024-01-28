69eba54eeb4ab2d717b7c80e97137c2130714f02
Aniol0012 <aniol0012@gmail.com>
2024-01-28 12:48:53 +0100

    fix: renamed moveSpeed to moveSpeedX

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     private float _horizontal;
     private static float DEFAULT_MOVE_SPEED = 5f;
     
-    [SerializeField] float moveSpeed = DEFAULT_MOVE_SPEED;
+    [SerializeField] float moveSpeedX = DEFAULT_MOVE_SPEED;
     public float groundCheckRadius = 0.2f;
     public float jumpForce;
     [SerializeField] private int maxJumps;
@@ -57,12 +57,12 @@ public class PlayerMovement : MonoBehaviour {
     private void HandleMovement() {
         _horizontal = Input.GetAxisRaw("Horizontal");
         if (isYoung) {
-            moveSpeed = DEFAULT_MOVE_SPEED;
+            moveSpeedX = DEFAULT_MOVE_SPEED;
         }
         else {
-            moveSpeed = DEFAULT_MOVE_SPEED * 1.5f;
+            moveSpeedX = DEFAULT_MOVE_SPEED * 1.5f;
         }
-        _rigidbody2D.velocity = new Vector2(_horizontal * moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = new Vector2(_horizontal * moveSpeedX, _rigidbody2D.velocity.y);
     }
 
     /**
